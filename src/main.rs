562fbe20d231a7f4ec4c44fb3d236d1111d48215
tu.perth <74957058+thanadet-u@users.noreply.github.com>
2025-07-26 00:15:10 +0700

    reading form inputs, canonical mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, World!");
+    /*
+       this programs reads from the standard input stream, then for each character(in this case only 1 byte) in it(b)
+       its ouput in a newline
+
+       note: the keyboard input is only sent when you press enter. canonical mode. not ideal for a text editor
+    */
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+
+        // when press read q, exits
+        if c == 'q' {
+            break;
+        }
+
+        println!("{}", c);
+    }
+
+    // TODO: we want the program to read the input immediately.
 }
