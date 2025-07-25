79e51478151ccc3645f50cfe62abf367c2fbe404
tu.perth <74957058+thanadet-u@users.noreply.github.com>
2025-07-26 00:32:06 +0700

    feat: enter raw mode using crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{self, Read};
 
 fn main() {
-    /*
-       this programs reads from the standard input stream, then for each character(in this case only 1 byte) in it(b)
-       its ouput in a newline
+    // so we use a library, crossterm to enter raw mode
 
-       note: the keyboard input is only sent when you press enter. canonical mode. not ideal for a text editor
-    */
-
+    enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
 
-        // when press read q, exits
+        // when read q, exits
         if c == 'q' {
+            // we only disable raw mode if we exit using q...
+            disable_raw_mode().unwrap();
             break;
         }
 
         println!("{}", c);
     }
-
-    // TODO: we want the program to read the input immediately.
 }
