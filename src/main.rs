d681fd6a03380b1d664469d924c7b7697a84fbfc
tu.perth <74957058+thanadet-u@users.noreply.github.com>
2025-07-28 00:35:55 +0700

    ch2, raw mode and error handling (unwrap)

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,31 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{self, Read};
 
 fn main() {
-    // so we use a library, crossterm to enter raw mode
-
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
+        // TODO: https://github.com/pflenker/hecto-tutorial/commit/23f1c0f6c54d794a8794fa40432c8e52103cc188
 
-        // when read q, exits
-        if c == 'q' {
-            // we only disable raw mode if we exit using q...
-            disable_raw_mode().unwrap();
-            break;
+        match b {
+            Ok(b) => {
+                // if b has no error assign it to b. (b is ok)
+
+                let c = b as char; // 'as' is type casting
+
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+
+                // when read q, exits
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+            } // if b can unwrap then unwrap err and print
         }
-
-        println!("{}", c);
     }
+    disable_raw_mode().unwrap(); // always disable raw mode
 }
