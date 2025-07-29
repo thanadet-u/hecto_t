bd552e2632423b3f2e1868db9d66fac991be7fc5
tu.perth <74957058+thanadet-u@users.noreply.github.com>
2025-07-29 20:28:27 +0700

    feat: my implementation of, assignment1: tildes, clearning the screen and writing ~ on each line.

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,9 @@
+use crossterm::cursor::MoveTo;
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
-use crossterm::execute;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::{ExecutableCommand, execute};
 use std::io::stdout;
+use std::io::{self, Write};
 
 pub struct Editor {
     should_quit: bool,
@@ -20,9 +22,10 @@ impl Editor {
     }
 
     fn initialize() -> Result<(), std::io::Error> {
-        // return Ok box with nothing in it, or return error
         enable_raw_mode()?; // ? unwrap the result, if its an error it return the error immediately
-        Self::clear_screen()
+        let res = Self::clear_screen();
+        let _ = Self::draw_rows();
+        res
     }
     fn terminate() -> Result<(), std::io::Error> {
         disable_raw_mode()
@@ -33,13 +36,13 @@ impl Editor {
     }
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
 
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(()) // if we get to this line then there should be no error so we return
     }
@@ -60,7 +63,28 @@ impl Editor {
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
             Self::clear_screen()?;
-            print!("Goodbye!\r\n");
+            println!("\r\nGoodbye!\r");
+        }
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let mut out = io::stdout();
+        // let size = size();
+        // let row: u16 = size.0;
+        // let col: u16 = size.1;
+        // dbg!(row);
+        // dbg!(col);
+        if let Ok(term_size) = size() {
+            // let col: u16 = term_size.0;
+            let row: u16 = term_size.1;
+            // println!("Rows: {row} \r");
+            // println!("Cols: {col} \r");
+            for r in 1..row {
+                out.execute(MoveTo(0, r))?;
+                write!(out, "~")?;
+            }
+            out.execute(MoveTo(0, 0))?;
         }
         Ok(())
     }
